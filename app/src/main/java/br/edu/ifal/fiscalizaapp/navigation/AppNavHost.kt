3c61cf649de0a20d9b803f659359cf2f390e87bb
ludmilabss <lbs11@aluno.ifal.edu.br>
2025-10-27 18:02:40 -0300

    feat: cria uma navegação (temporária) para visualizar a tela de listagem de categorias

--- app/src/main/java/br/edu/ifal/fiscalizaapp/navigation/AppNavHost.kt
+++ app/src/main/java/br/edu/ifal/fiscalizaapp/navigation/AppNavHost.kt
@@ -3,6 +3,11 @@ package br.edu.ifal.fiscalizaapp.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import br.edu.ifal.fiscalizaapp.screens.categories.CategoryListScreen
+
+const val categoryListRoute = "categories"
+
 
 @Composable
 fun AppNavHost(navController: NavHostController) {
@@ -10,5 +15,10 @@ fun AppNavHost(navController: NavHostController) {
         homeScreen(navController)
         exampleScreen(navController)
         loginScreen(navController)
+
+        composable(route = categoryListRoute) {
+            CategoryListScreen(navController = navController)
+
+        }
     }
 }
