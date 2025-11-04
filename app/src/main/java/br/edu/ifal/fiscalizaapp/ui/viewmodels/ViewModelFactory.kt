3f45131edc69827d0a41d188dec3c4e805f81a6b
ludmilabss <lbs11@aluno.ifal.edu.br>
2025-11-04 20:13:29 -0300

    refactor: remove função antiga que estava comentada

--- app/src/main/java/br/edu/ifal/fiscalizaapp/ui/viewmodels/ViewModelFactory.kt
+++ app/src/main/java/br/edu/ifal/fiscalizaapp/ui/viewmodels/ViewModelFactory.kt
@@ -33,13 +33,3 @@ class ViewModelFactory : ViewModelProvider.Factory {
         }
     }
 }
-
-/*override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(ProtocolViewModel::class.java)) {
-            val service = RetrofitHelper.getInstance().create(ModelService::class.java)
-            val repository = ProtocolRepository(service)
-            @Suppress("UNCHECKED_CAST")
-            return ProtocolViewModel(repository) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
-    }*/
